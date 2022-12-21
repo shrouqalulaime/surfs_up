df7c02f43d7570130eb0b4f54cbb51e790facb3a
Shrouq Alelaumi <shrouqalelaumi@Shrouqs-MacBook-Pro-2.local>
2022-12-21 10:38:54 -0800

    add anlaysis scrpit

---
+++ app2.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
